f005570e9efdb7a846802dfafd6728a041f5d5d6
breno085 <br3noacioli@gmail.com>
2025-03-17 04:41:04 -0300

    grafico de pizza para workshop

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,12 +1,9 @@
-// Função para mostrar os detalhes do workshop
 function mostrarDetalhes(workshopId) {
-    // Esconde todos os detalhes dos workshops
     var workshops = document.querySelectorAll('.detalhes');
     workshops.forEach(function(workshop) {
         workshop.style.display = 'none';
     });
 
-    // Exibe o workshop clicado
     var workshopDetalhes = document.getElementById(workshopId);
     workshopDetalhes.style.display = 'block';
 }
